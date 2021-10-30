3f3e95c141e6608ebc57965b883a734cff3e7b8e
ado24 <ado24@users.noreply.github.com>
2021-10-30 15:11:47 -0400

    Added for loop for processing Documents collection

--- weightsTrigger/run.ps1
+++ weightsTrigger/run.ps1
@@ -8,46 +8,49 @@ Write-Host "Doc count: $($Documents.Count)"
 #Write-Host "Doc1 count: $($Documents1.Count)"
 
 if ($Documents.Count -gt 0) {
+    for ($i = 0; $i -lt $Documents.Count; $i++) {
+        
     
-    $body = $Documents[0] | ConvertTo-Json
+        $body = $Documents[$i] | ConvertTo-Json
 
-    Write-Host "Body: Name:$($Documents[0].name)" 
-    Write-Host "Value: $($Documents[0].value)"
+        Write-Host "Body: Name:$($Documents[$i].name)" 
+        Write-Host "Value: $($Documents[$i].value)"
 
-    #$body = $body | ConvertFrom-Json
-    #$body | Add-Member -MemberType NoteProperty -Name "RowKey" -Value $Documents[0].id
-    #$body | Add-Member -MemberType NoteProperty -Name "PartitionKey" -Value $Documents[0].id
-    #$body.add("RowKey", $Documents[0].id)
-    #$body.RowKey = $Documents[0].id
+        #$body = $body | ConvertFrom-Json
+        #$body | Add-Member -MemberType NoteProperty -Name "RowKey" -Value $Documents[$i].id
+        #$body | Add-Member -MemberType NoteProperty -Name "PartitionKey" -Value $Documents[$i].id
+        #$body.add("RowKey", $Documents[$i].id)
+        #$body.RowKey = $Documents[$i].id
 
-    Write-Host "Body type: $($Documents[0].value.GetType())"
-    Write-Host "Converted output: $($body)"
-
-
-
-    # Associate values to output bindings by calling 'Push-OutputBinding'.
-    Push-OutputBinding -Name weightsTable -Value ($body) # | ConvertTo-Json)
+        Write-Host "Body type: $($Documents[$i].value.GetType())"
+        Write-Host "Converted output: $($body)"
 
 
-    $res = [HttpResponseContext]@{
-        StatusCode = [HttpStatusCode]::OK
-        #ContentType= "application/json"
-        #Body = ($body.ToString())
+
+        # Associate values to output bindings by calling 'Push-OutputBinding'.
+        Push-OutputBinding -Name weightsTable -Value ($body) # | ConvertTo-Json)
+
+
+        $res = [HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            #ContentType= "application/json"
+            #Body = ($body.ToString())
+        }
+        #Write-Host "Response: $($res.body)"    
+        #Push-OutputBinding -Name Response -Value [HttpResponseContext]$res
+        #Push-OutputBinding -Name res1 -Value $res
+
+        # Associate values to output bindings by calling 'Push-OutputBinding'.
+        # Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        #     StatusCode = [HttpStatusCode]::OK
+        #     Body = "Processed"
+        # }) -Clobber
     }
-    #Write-Host "Response: $($res.body)"    
-    #Push-OutputBinding -Name Response -Value [HttpResponseContext]$res
-    #Push-OutputBinding -Name res1 -Value $res
-
-    # Associate values to output bindings by calling 'Push-OutputBinding'.
-   # Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-   #     StatusCode = [HttpStatusCode]::OK
-   #     Body = "Processed"
-   # }) -Clobber
-   
-} else {
+}
+else {
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode = [HttpStatusCode]::ACCEPTED
-        Body = "No payload"
-    })
+            StatusCode = [HttpStatusCode]::ACCEPTED
+            Body       = "No payload"
+        })
     $res;
 }
