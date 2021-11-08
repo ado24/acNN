b5c6f024f25b013979b073f1e3eb215c219f4984
ado24 <ado24@users.noreply.github.com>
2021-11-08 17:57:49 -0500

    Removed commented out code

--- weightsTrigger/run.ps1
+++ weightsTrigger/run.ps1
@@ -16,16 +16,9 @@ if ($Documents.Count -gt 0) {
         Write-Host "Body: Name:$($Documents[$i].name)" 
         Write-Host "Value: $($Documents[$i].value)"
 
-        #$body = $body | ConvertFrom-Json
-        #$body | Add-Member -MemberType NoteProperty -Name "RowKey" -Value $Documents[$i].id
-        #$body | Add-Member -MemberType NoteProperty -Name "PartitionKey" -Value $Documents[$i].id
-        #$body.add("RowKey", $Documents[$i].id)
-        #$body.RowKey = $Documents[$i].id
 
         Write-Host "Body type: $($Documents[$i].value.GetType())"
         Write-Host "Converted output: $($body)"
-
-
 
         # Associate values to output bindings by calling 'Push-OutputBinding'.
         Push-OutputBinding -Name weightsTable -Value ($body) # | ConvertTo-Json)
@@ -34,18 +27,7 @@ if ($Documents.Count -gt 0) {
 
         $res = [HttpResponseContext]@{
             StatusCode = [HttpStatusCode]::OK
-            #ContentType= "application/json"
-            #Body = ($body.ToString())
         }
-        #Write-Host "Response: $($res.body)"    
-        #Push-OutputBinding -Name Response -Value [HttpResponseContext]$res
-        #Push-OutputBinding -Name res1 -Value $res
-
-        # Associate values to output bindings by calling 'Push-OutputBinding'.
-        # Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        #     StatusCode = [HttpStatusCode]::OK
-        #     Body = "Processed"
-        # }) -Clobber
     }
 }
 else {
